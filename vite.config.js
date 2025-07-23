7d5d84621677d910edde36b0141f488d2bb06ab1
Sayan Mandal <sm8304098@gmail.com>
2025-07-23 10:14:52 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/AI-ChatBot/', // 👈 VERY IMPORTANT for GitHub Pages!
   plugins: [react()],
-})
+});

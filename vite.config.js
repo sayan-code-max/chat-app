51d5b98219de11d10ff702b5bae87c6664685d61
Sayan Mandal <sm8304098@gmail.com>
2025-09-02 23:46:20 +0530

    fix: correct main.jsx entry in index.html

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
 
@@ -7,11 +8,43 @@
 // })
 
 
+=======
+//  HEAD
+>>>>>>> 4c9713a (first commit)
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
+
+// // import { defineConfig } from 'vite'
+// // import react from '@vitejs/plugin-react'
+
+// // // https://vite.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+
+
+
+
+
+
+// // 7d5d846 (Your commit message)
+// export default defineConfig({
+//   base: '/AI-ChatBot/', // 👈 VERY IMPORTANT for GitHub Pages!
+//   plugins: [react()],
+// });
+
+
+
+
+
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/AI-ChatBot/', // 👈 VERY IMPORTANT for GitHub Pages!
   plugins: [react()],
-});
+})
